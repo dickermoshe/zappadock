9b26438ef6878fbc8b1ed41d92ed726a2260e78e
Moshe Dicker <dickermoshe@gmail.com>
2022-02-20 07:08:52 +0000

    Change Verison

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="zappadock",
-    version='0.1.0',
+    version='0.1.1',
     author="Moshe Dicker",
     author_email='dickermoshe@gmail.com',
     description="A tool for running Zappa commands in a Lambda-like' environment.",
